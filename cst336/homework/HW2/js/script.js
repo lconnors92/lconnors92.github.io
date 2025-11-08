c95a58e80478a2adc4fb24a52c8a6b259d8420a5
Liam Connors <liam.m.connors@gmail.com>
2025-11-08 15:17:00 -0800

    question 4 finished

---
+++ cst336/homework/HW2/js/script.js
@@ -0,0 +1,81 @@
+//Even Listener(s)
+document.querySelector("button").addEventListener("click", gradeQuiz);
+
+//Global Variables
+var score = 0;
+
+//Functions
+function isFormValid() {
+    let isValid = true;
+    if(document.querySelector("#q1").value == "") {
+        isValid = false;
+        document.querySelector("#validationFdbk").innerHTML = "Question 1 was not answered.";
+    }
+    return isValid;
+}//isFormValid
+
+function rightAnswer(index) {
+    document.querySelector(`#q${index}Feedback`).innerHTML = "Correct!";
+    document.querySelector(`#q${index}Feedback`).className = "bg-success text-white";
+    document.querySelector(`#markImg${index}`).innerHTML = "<img src='img/checkmark.png' alt='Checkmark'>";
+    score += 20;
+}
+
+function wrongAnswer(index) {
+    document.querySelector(`#q${index}Feedback`).innerHTML = "Incorrect!";
+    document.querySelector(`#q${index}Feedback`).className = "bg-warning text-white";
+    document.querySelector(`#markImg${index}`).innerHTML = "<img src='img/xmark.png' alt='xmark'>";
+}
+
+
+function gradeQuiz() {
+    console.log("Grading quiz...");
+    document.querySelector("#validationFdbk").innerHTML = ""; //resets validation fdbk
+    if (!isFormValid()) {
+        return;
+    }
+
+    //variables
+    score = 0;
+    let q1Response = document.querySelector("#q1").value.toLowerCase();
+    let q2Response = document.querySelector("#q2").value;
+    let q4Response = document.querySelector("input[name=q4]:checked").value;
+
+    //Grading question 1
+    if (q1Response == "sacramento") {
+        rightAnswer(1);
+    }
+    else {
+        wrongAnswer(1);
+    }
+
+
+    //Grading question 2
+    if (q2Response == "mo") {
+        rightAnswer(2);
+    }
+    else {
+       wrongAnswer(2);
+    }
+
+    //Grading question 3
+    if (document.querySelector("#Jefferson").checked && document.querySelector("#Roosevelt").checked && 
+        !document.querySelector("#Jackson").checked && !document.querySelector("#Franklin").checked) {
+        rightAnswer(3);
+    }
+    else {
+       wrongAnswer(3);
+    }
+
+    // Grading question 4
+    if (q4Response == "Rhode Island") {
+        rightAnswer(4);
+    }
+    else {
+       wrongAnswer(4);
+    }
+
+    //Display quiz score:
+    document.querySelector("#totalScore").innerHTML = `Total Score: ${score}`;
+
+}//gradeQuiz
